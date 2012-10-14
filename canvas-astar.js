b454e7b18f66634362990c6f266443423be4589b
Daniel Imms <tyriar@tyriar.com>
2012-10-14 18:26:40 +1100

    Added licence and removed completed TODO comment

--- canvas-astar.js
+++ canvas-astar.js
@@ -1,4 +1,24 @@
-﻿(function(){
+﻿/* Copyright (C) 2012 Daniel Imms
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+(function () {
     var CANVAS_WIDTH = 960;
     var CANVAS_HEIGHT = 600;
     var COST_STRAIGHT = 1;
@@ -83,8 +103,6 @@
         var mouse = getPosition(e);
         mouse.x = Math.floor(mouse.x / MAP_SCALE);
         mouse.y = Math.floor(mouse.y / MAP_SCALE);
-
-        // TODO: Check if the point can be placed here (is on the map)
 
         for (var x = mouse.x - 2; x <= mouse.x + 2; x++) {
             for (var y = mouse.y - 2; y <= mouse.y + 2; y++) {
