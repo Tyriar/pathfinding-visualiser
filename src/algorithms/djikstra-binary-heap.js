9b591e5d25c75d6feb28b0db4c2220fa7b56d7ba
Tyriar <tyriar@tyriar.com>
2014-07-05 07:49:22 -0700

    Implement Djikstra
    Binary heap version implemented, there are issues with Fibonacci heap though
    so it's disabled on the UI.
    
    Fixes #4

---
+++ src/algorithms/djikstra-binary-heap.js
@@ -0,0 +1,75 @@
+/*! pathfinding-visualiser | (c) 2014 Daniel Imms | https://github.com/Tyriar/pathfinding-visualiser/blob/master/LICENSE */
+// UMD pattern: https://github.com/umdjs/umd/blob/master/returnExports.js
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define(['core', 'canvas-helper', 'binary-heap', 'map-node', 'djikstra-common'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('./core'), require('./canvas-helper'), require('./binary-heap'), require('./map-node'), require('./djikstra-common'));
+  } else {
+    root.djikstraBinaryHeap = factory(core, canvasHelper, BinaryHeap, MapNode, aStarCommon);
+  }
+}(this, function (core, canvasHelper, BinaryHeap, MapNode, djikstraCommon) {
+  'use strict';
+
+  var module = {};
+
+  module.run = function (map, callback) {
+    var dist = {};
+    var queue = new BinaryHeap();
+    var queueNodes = {};
+    var queuedPaints = [];
+    var key, x, y, i;
+
+    dist[map.start.x + ',' + map.start.y] = 0;
+    queue.insert(0, map.start);
+    for (x = 0; x < map.width; x++) {
+      for (y = 0; y < map.height; y++) {
+        key = x + ',' + y;
+        if (dist[key] === undefined) {
+          dist[key] = Number.MAX_VALUE;
+        }
+        var node = new MapNode(x, y);
+        var heapNode = queue.insert(dist[key], node);
+        queueNodes[key] = heapNode;
+      }
+    }
+
+    while (!queue.isEmpty()) {
+      var min = queue.extractMinimum();
+      var neighbours = djikstraCommon.getNeighbourNodes(map, min.value, queueNodes);
+      for (i = 0; i < neighbours.length; i++) {
+        key = neighbours[i].x + ',' + neighbours[i].y;
+        var alt = dist[min.value.x + ',' + min.value.y] + neighbours[i].g;
+        if (alt < dist[key]) {
+          dist[key] = alt;
+          neighbours[i].parent = min.value;
+          if (neighbours[i].equals(map.goal)) {
+            var finish = performance.now();
+            var visitedNodeCount = 0;
+            var distKeys = Object.keys(dist);
+            for (var j = 0; j < distKeys.length; j++) {
+              if (dist[distKeys[j]] < Number.MAX_VALUE) {
+                visitedNodeCount++;
+              }
+            }
+            var message = djikstraCommon.buildSummaryMessage(map, visitedNodeCount);
+            callback(message, queuedPaints, neighbours[i], [], finish);
+            return;
+          }
+
+          queuedPaints.push({
+            f: canvasHelper.drawVisited,
+            x: neighbours[i].x,
+            y: neighbours[i].y
+          });
+          queue.decreaseKey(queueNodes[key], alt);
+        }
+      }
+    }
+
+    callback([{ result: 'No path exists' }], queuedPaints, undefined, undefined, performance.now());
+  };
+
+  return module;
+}));
---
+++ src/algorithms/djikstra-common.js
@@ -0,0 +1,52 @@
+/*! pathfinding-visualiser | (c) 2014 Daniel Imms | https://github.com/Tyriar/pathfinding-visualiser/blob/master/LICENSE */
+// UMD pattern: https://github.com/umdjs/umd/blob/master/returnExports.js
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define(['canvas-helper', 'map-node'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('canvas-helper'), require('map-node'));
+  } else {
+    root.djikstraCommon = factory(canvasHelper, MapNode);
+  }
+}(this, function (canvasHelper, MapNode) {
+  'use strict';
+
+  var COST_STRAIGHT = 1;
+  var COST_DIAGONAL = 1.414;
+
+  var module = {};
+
+  module.getNeighbourNodes = function (map, node, queueNodes) {
+    var neighbours = [];
+
+    for (var x = node.x - 1; x <= node.x + 1; x++) {
+      for (var y = node.y - 1; y <= node.y + 1; y++) {
+        if (x === node.x && y === node.y) {
+          continue;
+        }
+        if (!map.isOnMap(x, y) || !map[x][y]) {
+          continue;
+        }
+        var cost = (x === node.x || y === node.y ? COST_STRAIGHT : COST_DIAGONAL);
+        var key = x + ',' + y;
+        var neighbour = queueNodes[key].value;
+        neighbour.g = cost;
+        neighbours.push(neighbour);
+      }
+    }
+
+    return neighbours;
+  };
+
+  module.buildSummaryMessage = function (map, visitedNodeCount) {
+    return [{
+      result: 'Map size = ' + map.width + 'x' + map.height
+    }, {
+      result: 'Number of visited nodes = ' + visitedNodeCount,
+      colour: canvasHelper.CLOSED_LIST_COLOUR
+    }];
+  };
+
+  return module;
+}));
