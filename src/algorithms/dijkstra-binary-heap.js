eec938d9d9658b71ec9ac0e48184930e68e8b315
Tyriar <tyriar@tyriar.com>
2014-07-05 13:11:36 -0700

    Merge dijkstra heap algorithms
    Fixes #43

--- src/algorithms/dijkstra-binary-heap.js
+++ src/algorithms/dijkstra-binary-heap.js
@@ -2,75 +2,18 @@
 (function (root, factory) {
   'use strict';
   if (typeof define === 'function' && define.amd) {
-    define(['core', 'canvas-helper', 'binary-heap', 'map-node', 'dijkstra-common'], factory);
+    define(['dijkstra-heap', 'fibonacci-heap'], factory);
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('../core'),
-                             require('../canvas-helper'),
-                             require('../../bower_components/js-data-structures/src/binary-heap'),
-                             require('../map-node'),
-                             require('./dijkstra-common'));
+    module.exports = factory(require('./dijkstra-heap'),
+                             require('../../bower_components/js-data-structures/src/binary-heap'));
   }
-}(this, function (core, canvasHelper, BinaryHeap, MapNode, dijkstraCommon) {
+}(this, function (dijkstraHeap, BinaryHeap) {
   'use strict';
 
   var module = {};
 
   module.run = function (map, callback) {
-    var distance = {};
-    var queue = new BinaryHeap();
-    var queueNodes = {};
-    var queuedPaints = [];
-    var x, y, i;
-
-    var startKey = map.start.getHashKey();
-    distance[startKey] = 0;
-
-    queue.insert(0, map.start);
-
-    while (!queue.isEmpty()) {
-      var min = queue.extractMinimum();
-      var minKey = min.value.getHashKey();
-      var neighbours = dijkstraCommon.getNeighbourNodes(map, min.value, queueNodes);
-
-      for (i = 0; i < neighbours.length; i++) {
-        var neighbour = neighbours[i];
-        var neighbourKey = neighbour.getHashKey();
-        var alt = distance[minKey] + neighbour.g;
-
-        if (typeof queueNodes[neighbourKey] === 'undefined' &&
-            (typeof distance[neighbourKey] === 'undefined' || alt < distance[neighbourKey])) {
-          distance[neighbourKey] = alt;
-          neighbour.parent = min.value;
-
-          if (neighbour.equals(map.goal)) {
-            var finish = core.timeNow();
-            var visitedNodeCount = 0;
-            var distanceKeys = Object.keys(distance);
-            for (var j = 0; j < distanceKeys.length; j++) {
-              if (distance[distanceKeys[j]] < Number.MAX_VALUE) {
-                visitedNodeCount++;
-              }
-            }
-            var message = dijkstraCommon.buildSummaryMessage(map, visitedNodeCount);
-            callback(message, queuedPaints, neighbour, [], finish);
-            return;
-          }
-
-          if (typeof queueNodes[neighbourKey] === 'undefined') {
-            queuedPaints.push({
-              f: canvasHelper.drawVisited,
-              x: neighbours[i].x,
-              y: neighbours[i].y
-            });
-            queue.insert(alt, neighbour);
-          } else {
-            queue.decreaseKey(queueNodes[neighbourKey], alt);
-          }
-        }
-      }
-    }
-
-    callback([{ result: 'No path exists' }], queuedPaints, undefined, undefined, core.timeNow());
+    dijkstraHeap.run(map, callback, BinaryHeap);
   };
 
   return module;
