38c0d24fd59c84039d427444b4cc92fbedfcb491
Tyriar <tyriar@tyriar.com>
2014-07-18 14:43:10 -0700

    Start on toolbar

---
+++ polymer_components/pv-toolbar.js
@@ -0,0 +1,9 @@
+/*! pathfinding-visualiser | (c) 2014 Daniel Imms */
+/*! https://github.com/Tyriar/pathfinding-visualiser/blob/master/LICENSE */
+(function () {
+  'use strict';
+
+  Polymer('pv-toolbar', {
+
+  });
+})();
