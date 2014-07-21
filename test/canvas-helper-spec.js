f06554aacb0e6d24ff6e6687adec26f5ee3db00a
Daniel Imms <tyriar@tyriar.com>
2014-07-21 02:28:28 -0700

    Add canvas test

--- test/canvas-helper-spec.js
+++ test/canvas-helper-spec.js
@@ -9,7 +9,7 @@ describe('canvas-helper', function () {
     canvasHelper = require('../src/canvas-helper');
     context = {
       beginPath: function () {},
-      fillPath: function () {},
+      fillRect: function () {},
       fillStyle: function () {},
       lineTo: function () {},
       moveTo: function () {},
@@ -28,4 +28,18 @@ describe('canvas-helper', function () {
   });
 
   // TODO: Implement tests
+  describe('drawNode', function () {
+    it('should draw a node at (x, y) position provided', function () {
+      spyOn(context, 'fillRect');
+      canvasHelper.drawNode(2, 3);
+      expect(context.fillRect).toHaveBeenCalled();
+      expect(context.fillRect.callCount).toBe(1);
+      // Default scale of 10
+      expect(context.fillRect.argsForCall[0]).toEqual([20, 30, 10, 10]);
+    });
+
+    // TODO: Pull MAP_SCALE in to canvas-helper or make it a parameter
+    //it('should draw based on map scale', function () {
+    //});
+  });
 });
